23cbfd2278e73cfcf713d89b17e151de875117bb
Adeleke <kamade123@gmail.com>
2022-03-10 14:14:35 +0100

    api completed

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require('dotenv').config()
+require('express')
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const  notFoundMiddleware = require('./middleware/not-found')
+const  errorHandler = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res)=>{
+  res.send('<h1>Store API</h1> <a href="/api/vi/products"> route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+// product routes
+app.use(notFoundMiddleware)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+  try {
+    await connectDB(process.env.MONGO_DEV)
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  } catch(error) {
+    console.log(error)
+  }
+}
+start()
